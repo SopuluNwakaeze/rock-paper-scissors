abbba892b488288018c047754e1e9e064fbb604b
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-07-16 19:59:33 -0600

    Added computersChoice() function
    I added the logic that makes the computer able to
    make a random guess.

--- index-script.js
+++ index-script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+function computersChoice(){
+    var choiceArray = ["rock","paper","scissors"];
+    var random = Math.random();
+    if(random <= 0.3333333333333333){
+        return choiceArray[0];
+    }
+    if( 0.666666666666666 >= random >= 0.333333333333333){
+        return choiceArray[1];
+    }
+    else{
+        return choiceArray[2];
+    }
+}
+
+console.log(computersChoice())
