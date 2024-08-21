5363f4f4e3b38363b40eb42b736012d2f26f7417
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-08-21 11:34:40 -0600

    Added a div to display the score
    I removed the functionality of using the console to display the score
    now I am using a div that is updated using DOM maniputation.

--- index-script.js
+++ index-script.js
@@ -1,6 +1,7 @@
 const MESSAGE = "Please enter rock, paper, or scissors";
 const choiceArray = ["rock","paper","scissors"];
 const buttons = document.querySelectorAll(".btn");
+const div = document.querySelector("#div");
 var value = "";
 
 buttons.forEach((button) => { 
@@ -42,38 +43,56 @@ function getHumansChoice(){
 function playRound(humanChoice, computerChoice){
     var humanScore = 0;
     var computerScore = 0;
+    //const span = document.createAttribute("span");
     if(humanChoice === "rock" && computerChoice === "rock"){
-        console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+        //console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "rock" && computerChoice === "paper"){
         computerScore++;
-        console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "rock" && computerChoice === "scissors"){
         humanScore++;
-        console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "paper" && computerChoice === "rock"){
         humanScore++;
-        console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "paper" && computerChoice === "paper"){
-        console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "paper" && computerChoice === "scissors"){
         computerScore++;
-        console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "scissors" && computerChoice === "rock"){
         computerScore++;
-        console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Computer won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "scissors" && computerChoice === "paper"){
         humanScore++;
-        console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`Human won the round! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
     if(humanChoice === "scissors" && computerChoice === "scissors"){
-        console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
+        div.textContent = `The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`;
+
+        //console.log(`The round was a tie! human picked ${humanChoice} and computer picked ${computerChoice} the score is Human: ${humanScore} Computer: ${computerScore}`)
     }
 }
 
